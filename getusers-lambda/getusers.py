8c10d9b3d68540168cdf1a6865bc1707ef796984
Siddhant <siddhant1042@gmail.com>
2019-09-23 17:09:23 +0530

    Get Last 5 Memes

--- getusers-lambda/getusers.py
+++ getusers-lambda/getusers.py
@@ -3,14 +3,24 @@ import logging
 import os
 from botocore.exceptions import ClientError
 import boto3
+from datetime import date
+from boto3.dynamodb.conditions import Key, Attr
+
+
+dynamodb = boto3.resource('dynamodb')
 
 def get_users():
-    #TODO
-    pass 
+    pass
 
 def get_memes():
-    #TODO
-    pass
+    today = date.today()
+    current_day = today.strftime("%d/%m/%Y")
+    memes_table = dynamodb.Table('Memes')
+    results = memes_table.query(Select="SPECIFIC_ATTRIBUTES",ScanIndexForward=True,KeyConditionExpression=Key("Meme_ID").eq(current_day),FilterExpression=Attr('Category').eq('default'),ProjectionExpression="Author,ContentUrl,PostUrl,Caption,Category",Limit=5)
+    memes = results['Items']
+    if len(memes)==0:
+        raise Exception('DB returned no results')
+    return memes
 
 
 def lambda_handler(event, context):
