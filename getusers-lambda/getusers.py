9c35dd66449bd7a705cc0a657b187c243aebf3f6
Siddhant <siddhant1042@gmail.com>
2019-09-24 01:05:20 +0530

    Get Only Active Users & Minor Fixes

--- getusers-lambda/getusers.py
+++ getusers-lambda/getusers.py
@@ -7,24 +7,19 @@ from datetime import date
 from boto3.dynamodb.conditions import Key, Attr
 
 logger = logging.getLogger()
-logger.setLevel(logging.info)
+logger.setLevel(logging.INFO)
 
 dynamodb = boto3.resource('dynamodb')
 
 def get_users():
     users_table = dynamodb.Table('Chats')
     subscribers = []
-    try:
-        chats = users_table.scan(AttributesToGet=['chatid']) #TODO check for status is active 
-        logger.info(f"chats type is {type(chats)} and value is {chats}")
-        for items in chats['Items']:
-            chat_id = items['chatid']
-            subscribers.append(chat_id)
-        logger.info(f"all subs are {subscribers}")
-    except:
-        logger.error("couldn't query db to get chats")
-        raise Exception('Error occured while scanning for chatids')
-    
+    chats = users_table.scan(ProjectionExpression='chatid,user_status',FilterExpression=Attr('user_status').eq(True)) 
+    logger.info(f"chats type is {type(chats)} and value is {chats}")
+    for items in chats['Items']:
+        chat_id = items['chatid']
+        subscribers.append(chat_id)
+    logger.info(f"all chats are {subscribers}")
     if len(subscribers)==0:
         logger.error("found no chat ids")
         raise Exception('No chat ids')
@@ -36,7 +31,7 @@ def get_memes():
     current_day = today.strftime("%d/%m/%Y")
     memes_table = dynamodb.Table('Memes')
     logger.info("Looking for memes in the table")
-    results = memes_table.query(Select="SPECIFIC_ATTRIBUTES",ScanIndexForward=True,KeyConditionExpression=Key("Meme_ID").eq(current_day),FilterExpression=Attr('Category').eq('default'),ProjectionExpression="Author,ContentUrl,PostUrl,Caption,Category",Limit=5)
+    results = memes_table.query(Select="SPECIFIC_ATTRIBUTES",ScanIndexForward=False,KeyConditionExpression=Key("Meme_ID").eq(current_day),FilterExpression=Attr('Category').eq('default'),ProjectionExpression="Author,ContentUrl,PostUrl,Caption,Category",Limit=5)
     logger.info(f"Result is f{results}")
     memes = results['Items']
     if len(memes)==0:
@@ -59,6 +54,6 @@ def lambda_handler(event, context):
         response = lambda_client.invoke(
             FunctionName="sendMemes",
             InvocationType='Event',
-            Payload=memes
+            Payload=data
         )
         logger.info(f"Invoked sendMemes Lambda with response f{response}")
